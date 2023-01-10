2e191dcc07ec81c98112d3425d9f25463dc769bf
JESGOD <yonnyes@unicauca.edu.co>
2023-01-10 12:09:52 -0500

    se decoro el menu de secciones de la galeria

--- src/Main.jsx
+++ src/Main.jsx
@@ -15,29 +15,48 @@ import { Preguntas } from "./Preguntas";
 /* fin preguntas frecuentes */
 
 /* galeria */
-import  Galeria  from "./Galeria";
+import Galeria from "./Galeria";
 /* fin galeria */
+
+
+/* js menu secciones galeria */
+
+
+/* fin js menu secciones galeria */
 export function Main() {
   return (
     <main>
       <header>
         <SwiperHeader />
       </header>
-      <main className="contenedorP" >
+      <main className="contenedorP">
         <section className="swiperArticulosNuevos ">
           <div className="textoArticulosNuevos">
-            <h2 className="subtitulo">New unique design and exclusive collection</h2>
+            <h2 className="subtitulo">
+              New unique design and exclusive collection
+            </h2>
           </div>
           <ArticulosNuevos />
         </section>
+        <div className="secciones-galeria">
+          <ul className="opciones-galeria">
+            <li className="opciones"><a href="#" className="subtitulo">Abstract art</a> </li>
+            <li className="opciones"><a href="#" className="subtitulo">Pixel Art</a> </li>
+          </ul>
+        </div>
         <sectiom className="contenedor-galeria">
-        <h1 className="subtitulo">Art gallery</h1>
-          <Galeria/> 
+          {/* <h1 className="subtitulo">Art gallery</h1> */}
+          <section className="seccion-arteAbstracto">
+          <Galeria />
+          </section>
+          <section className="seccion-pixelArt">
+            <h1>ejemplo</h1>
+          </section>
+          
         </sectiom>
         <section className="preguntas-frecuentes">
-          <Preguntas/>
+          <Preguntas />
         </section>
-        
       </main>
     </main>
   );
